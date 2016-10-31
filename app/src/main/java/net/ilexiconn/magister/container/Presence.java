266e621d5ca0f0fc5fb954e4ffcb59da87e6b4db
Z3r0byte <basvandenboom3@gmail.com>
2016-10-31 19:19:04 +0100

    updated copyright

--- app/src/main/java/net/ilexiconn/magister/container/Presence.java
+++ app/src/main/java/net/ilexiconn/magister/container/Presence.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Bas van den Boom 'Z3r0byte'
+ * Copyright (c) 2016-2016 Bas van den Boom 'Z3r0byte'
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
