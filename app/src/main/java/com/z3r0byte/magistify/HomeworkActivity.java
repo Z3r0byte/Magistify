5b0040b0d25e95bcdd20dadd6d25cd86a91e4fd7
Z3r0byte <z3r0byte.apps@gmail.com>
2017-10-17 19:36:39 +0200

    Created empty homework activity

---
+++ app/src/main/java/com/z3r0byte/magistify/HomeworkActivity.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2016-2017 Bas van den Boom 'Z3r0byte'
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.z3r0byte.magistify;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.google.gson.Gson;
+import com.z3r0byte.magistify.Fragments.HomeworkFragment;
+import com.z3r0byte.magistify.GUI.NavigationDrawer;
+import com.z3r0byte.magistify.Util.ConfigUtil;
+
+import net.ilexiconn.magister.container.Profile;
+import net.ilexiconn.magister.container.User;
+
+import it.neokree.materialtabs.MaterialTabHost;
+
+public class HomeworkActivity extends AppCompatActivity {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    Toolbar mToolbar;
+    MaterialTabHost tabHost;
+    ViewPager viewPager;
+    PagerAdapter pagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homework);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle(R.string.title_activity_homework);
+        setSupportActionBar(mToolbar);
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+
+        ConfigUtil configUtil = new ConfigUtil(this);
+
+        User user = new Gson().fromJson(configUtil.getString("User"), User.class);
+        Profile profile = new Gson().fromJson(configUtil.getString("Profile"), Profile.class);
+
+        NavigationDrawer navigationDrawer = new NavigationDrawer(this, mToolbar,
+                profile, user, "Homework");
+        navigationDrawer.SetupNavigationDrawer();
+
+    }
+
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if (position == 0) {
+                return new HomeworkFragment();
+            } else {
+                return new HomeworkFragment();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 1;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Huiswerk";
+                case 1:
+                    return "Instellingen";
+            }
+            return null;
+        }
+    }
+}
